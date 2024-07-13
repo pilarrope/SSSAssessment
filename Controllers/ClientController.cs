7de821dcd95998a978d5016d333a04c58e92150f
Pilar Rodriguez <ec1405423@edinburghcollege.ac.uk>
2024-07-13 19:23:55 +0100

    Pagination added in Client page

--- Controllers/ClientController.cs
+++ Controllers/ClientController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using SSSAssessment.Models;
 
 namespace SSSAssessment.Controllers
@@ -12,13 +13,46 @@ namespace SSSAssessment.Controllers
             _httpClientFactory = httpClientFactory;
         }
 
-        //Accion for the Index page: localhost+ /Clients
-        public async Task<IActionResult> Index()
+        //Accion for the Index page: localhost+ /Clients/Clients
+        public async Task<IActionResult> Clients(int page = 1)
         {
-            var client = _httpClientFactory.CreateClient();
-            var apiResult = await client.GetFromJsonAsync<ApiResult>("https://tech-exercise.vercel.app/api/user");
-            return View(apiResult.Results);
+            
+            int pageSize = 15;// Clients per page.
+            List<Client> allClients = new List<Client>();
+            int currentPage = 1;
+            bool moreClients = true;
+
+            while (moreClients)
+            {
+                var apiResult = await _httpClientFactory.CreateClient().GetFromJsonAsync<ApiResult>($"https://tech-exercise.vercel.app/api/user?page={currentPage}&limit={pageSize}");
+
+
+                if (apiResult == null || apiResult.Results == null || apiResult.Results.Count == 0)
+                {
+
+                    moreClients = false; // Stop the loop if no more data is available.
+                }
+                else
+                {
+                    allClients.AddRange(apiResult.Results);
+                    currentPage++; // Increment to reach the next page.
+                    if (apiResult.Results.Count < pageSize)
+                    {
+                        moreClients = false; // Last page. When there is not more Clients.
+                    }
+                }
+            }
+
+
+
+            int totalClientCount = allClients.Count; // This will count the total of clients
+
+            var clientsToShow = allClients.Skip((page - 1) * pageSize).Take(pageSize).ToList();
+            var listPagination = new ListPagination<Client>(clientsToShow, totalClientCount, page, pageSize);
+
+            return View("Clients", listPagination);
         }
+
 
         //Acction for the Details page: localhost + Clients/Details/ + User  User example: b862e179-c346-4a2a-a913-75ef09d240e5
         public async Task<IActionResult> Details(string id)
---
+++ Models/ListPagination.cs
@@ -0,0 +1,26 @@
+﻿namespace SSSAssessment.Models
+{
+    public class ListPagination<T> : List<T>
+    {
+        public int PageIndex { get; private set; }
+        public int TotalPages { get; private set; }
+
+        public ListPagination(List<T> items, int count, int pageIndex, int pageSize)
+        {
+            PageIndex = pageIndex;
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+
+            this.AddRange(items);
+        }
+
+        public bool HasPreviousPage => PageIndex > 1;
+        public bool HasNextPage => PageIndex < TotalPages;
+
+        public static ListPagination<T> Create(IQueryable<T> source, int pageIndex, int pageSize)
+        {
+            var count = source.Count();
+            var items = source.Skip((pageIndex - 1) * pageSize).Take(pageSize).ToList();
+            return new ListPagination<T>(items, count, pageIndex, pageSize);
+        }
+    }
+}
