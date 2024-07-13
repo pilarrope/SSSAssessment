329e3ab3055c01da388d4501e2e9150903d2db17
Pilar Rodriguez <ec1405423@edinburghcollege.ac.uk>
2024-07-13 15:12:56 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SSSAssessment.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
